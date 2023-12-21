fa1fe240c3fab8e8f30e682db4021d5658036d49
JATIN <jatin080402@gmail.com>
2023-12-21 15:22:10 +0530

    21/12

---
+++ rejoice/script.js
@@ -0,0 +1,38 @@
+function cursorEffect(){
+    var page1Content = document.querySelector("#page1-content")
+    var cursor = document.querySelector("#cursor")
+
+    page1Content.addEventListener("mousemove",function(dets){
+        // console.log("lalala")
+        // console.log(dets)
+
+        // cursor.style.left = dets.x + "px"
+        // cursor.style.top = dets.y + "px"
+
+        gsap.to(cursor,{
+            x:dets.x,
+            y:dets.y
+        })
+    })
+
+    page1Content.addEventListener("mouseenter",function(){
+        gsap.to(cursor,{
+            scale:1,
+            opacity:1
+        })
+    })
+
+    page1Content.addEventListener("mouseleave",function(){
+        gsap.to(cursor,{
+            scale:0,
+            opacity:0
+        })
+    })
+
+    // gsap.to("#cursor",{
+    //     x:500,
+    //     y:500
+    // })
+}
+
+cursorEffect()
